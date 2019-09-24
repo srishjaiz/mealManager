f0d511dcbacec0eeb8d118daeadc222d390698b5
srishjaiz <34750718+srishjaiz@users.noreply.github.com>
2019-09-24 16:05:31 +0530

    inital commit key.js

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     google: {
-        clientID: '/*your client ID here*/',
-        clientSecret: '/*your client secret here*/'
+        clientID: '60192020373-7nrohq8fclp05u8r27pakrq5m2q0hmnq.apps.googleusercontent.com',
+        clientSecret: '6QxEXYQ8kaoDAfA7TSSMO7S9'
     }
 };
