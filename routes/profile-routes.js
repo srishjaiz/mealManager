75f1e06582a45b4baabc7e60fc8281ad475402ec
Srishti <srishjaiz@gmail.com>
2019-09-29 05:15:56 +0530

    add guest working as expected!

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const User = require('../models/user-model');
 
 const authCheck = (req, res, next) => {
     if(!req.user){
@@ -25,10 +26,44 @@ router.get('/userprofile', authCheck, (req, res) => {
 router.post('/usermanage', authCheck, (req, res) => {
     console.log("body: ",req.body.guestEmail);
     console.log("user: ",req.user);
+    // try {
+    //     db.products.insertOne( { _id: 10, "item" : "packing peanuts", "qty" : 200 } );
+    //  } catch (e) {
+    //     print (e);
+    //  }
     res.writeHead(200, {
         'Content-Type': 'application/json'
     });
-    res.end(JSON.stringify(req.user));
+    User.findOne({useremail: req.body.guestEmail}).then((userExists)=>{
+        if(userExists){
+            let data={
+                user: userExists,
+                exists: true
+            }
+            // console.log(data);
+            res.end(JSON.stringify(data));            
+        }
+        else{
+            //new guest must be added
+            User.create(
+                new User({
+                    useremail: req.body.guestEmail
+                }))
+            .then((newUser)=>{
+                if(newUser){
+                    console.log("data inserted", newUser);
+                    let data={
+                        user: newUser,
+                        exists: false
+                    }
+                    res.end(JSON.stringify(data));
+                }
+                else{
+                    console.log("insertion failed!");
+                }
+            });
+        }
+    });
 });
 
 module.exports = router;
